803f3c9c681b8ea89cb5e352cdb2de34bb680a59
sillas.braga <sillas.braga@sankhya.com.br>
2025-12-04 10:36:15 -0300

    corrigindo o tipo da importação

--- src/app/functions/upload-image.ts
+++ src/app/functions/upload-image.ts
@@ -3,7 +3,7 @@ import z from 'zod'
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
 import { uploadFileToStorage } from '@/infra/storage/upload-file-to-storage'
-import { Either, makeLeft, makeRight } from '@/shared/either'
+import { type Either, makeLeft, makeRight } from '@/shared/either'
 import { InvalidFileFormat } from './errors/invalid-file-format'
 
 const uploadImageInput = z.object({
